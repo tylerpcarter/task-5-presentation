3559af249dfef0abe83667a58cd69bca6707a146
tylercartergse2013 <tylercartergse2013@gmail.com>
2017-07-27 12:44:32 -0400

    deleted index

--- build-presentation.sh
+++ build-presentation.sh
@@ -5,4 +5,4 @@ TEMPLATE=template-index.html
 TRANSITION=slide
 THEME=black
 
-pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" reveal.md -o revealhtml.html
+pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" reveal.md -o index.html
